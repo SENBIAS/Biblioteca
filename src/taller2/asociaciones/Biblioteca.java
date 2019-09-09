95dbfcfb7902d8603bad0db5eaf7660cb99417f9
SENBIAS <49045371+SENBIAS@users.noreply.github.com>
2019-09-08 22:08:59 -0500

    Proyecto inconcluso en este commit. Se debe mejorar el metodo de la clse biblioteca el cual me permitira agregar nuevos libros a la biblioteca.
    Adicional a esto debe crearse el metodo CRUD, y terminar la clase Libro

---
+++ src/taller2/asociaciones/Biblioteca.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller2.asociaciones;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Biblioteca {
+    private String nombre;
+    private String direccion;
+    private ArrayList<Libro> listaLibros;
+
+    public Biblioteca() {
+        this.listaLibros = new ArrayList<>();
+        this.direccion = null;
+        this.nombre=null;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public ArrayList<Libro> getListaLibros() {
+        return listaLibros;
+    }
+
+    public void setListaLibros(ArrayList<Libro> listaLibros) {
+        this.listaLibros = listaLibros;
+    }
+
+    
+    
+    
+}
