622a6819b2d4386fcf2576e392f31725850c49e9
SENBIAS <49045371+SENBIAS@users.noreply.github.com>
2019-09-08 22:13:54 -0500

    Proyecto inconcluso en este commit. Se debe mejorar el metodo de la clse biblioteca el cual me permitira agregar nuevos libros a la biblioteca.
    Adicional a esto debe crearse el metodo CRUD, y terminar la clase Libro

---
+++ src/taller2/asociaciones/Libro.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller2.asociaciones;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Libro {
+    private String titulo;
+    private String autor;
+    private int isbn;
+    private int pages;
+    private String fecha;
+
+    public Libro(String titulo, String autor, int isbn, int pages, String fecha) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.pages = pages;
+        this.fecha = fecha;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+}
